26f13c974b3118b6aa8281bd22bd9c61aeb2c8c8
Mumin Bashir Baba <mumin@pragtex.com>
2026-03-03 18:07:52 +0530

    feat: setup turborepo

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,20 +1,14 @@
 import { Button } from "@workspace/ui/components/button";
-import { Input } from "@workspace/ui/components/input";
-import { add } from "@workspace/math/add";
-import { subtract } from "@workspace/math/subtract";
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello app/web</h1>
+        <h1 className="text-2xl font-bold">Hello app/widget</h1>
         <div className="flex gap-2">
           <Button>Button</Button>
           <Button variant="outline">Outline</Button>
         </div>
-        <p>{add(2, 3)}</p>
-        <p>{subtract(5, 3)}</p>
-        <Input placeholder="Input" />
       </div>
     </div>
   );
