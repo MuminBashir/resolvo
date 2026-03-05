28ef149b2364d7a4d4aea36eba5bccf633cf1fdd
Mumin Bashir <94466348+MuminBashir@users.noreply.github.com>
2026-03-05 22:36:36 +0530

    TASK-4_feat_add_organization_layouts

--- apps/web/app/(auth)/layout.tsx
+++ apps/web/app/(auth)/layout.tsx
@@ -1,11 +1,8 @@
+import { AuthLayout } from "@/modules/auth/ui/layouts/auth-layout";
 import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="min-h-screen min-w-screen flex flex-col justify-center items-center">
-      {children}
-    </div>
-  );
+  return <AuthLayout>{children}</AuthLayout>;
 };
 
 export default layout;
---
+++ apps/web/app/(dashboard)/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useMutation, useQuery } from "convex/react";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { api } from "@workspace/backend/_generated/api";
+import { Button } from "@workspace/ui/components/button";
+
+export default function Page() {
+  const users = useQuery(api.users.getMany);
+  const addUser = useMutation(api.users.add);
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-svh gap-4">
+        <UserButton />
+        <OrganizationSwitcher hidePersonal />
+        <h1 className="text-2xl font-bold">Hello app/web</h1>
+        <Button
+          onClick={() => {
+            addUser();
+          }}
+        >
+          Add User
+        </Button>
+        <div className="max-w-sm mx-auto w-full">{JSON.stringify(users)}</div>
+      </div>
+    </>
+  );
+}
