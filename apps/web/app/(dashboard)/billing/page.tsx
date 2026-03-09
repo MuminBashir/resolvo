eadf97d771718ba8d8b98c050792b95359be3d68
Mumin Bashir Baba <mumin@pragtex.com>
2026-03-09 16:52:59 +0530

    TASK-7_feat_create_dashboard_sidebar

---
+++ apps/web/app/(dashboard)/billing/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div>Plans & Billing</div>;
+};
+
+export default Page;
---
+++ apps/web/app/(dashboard)/customization/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div>Widget Customization</div>;
+};
+
+export default Page;
