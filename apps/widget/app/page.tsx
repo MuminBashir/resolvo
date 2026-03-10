598bda7bf89eac0adb20e13c4842e6dfc1532c44
Mumin Bashir Baba <mumin@pragtex.com>
2026-03-10 15:18:49 +0530

    TASK-9_feat_setup_widget_layout

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,43 +1,14 @@
-"use client";
+import { WidgetView } from "@/modules/widget/ui/views/widget-view";
+import { use } from "react";
 
-import { useVapi } from "@/modules/widget/hooks/use-vapi";
-import { Button } from "@workspace/ui/components/button";
+interface Props {
+  searchParams: Promise<{
+    organizationId: string;
+  }>;
+}
 
-export default function Page() {
-  const {
-    startCall,
-    endCall,
-    isConnecting,
-    isConnected,
-    isSpeaking,
-    transcript,
-  } = useVapi();
+export default function Page({ searchParams }: Props) {
+  const { organizationId } = use(searchParams);
 
-  return (
-    <div className="flex flex-col items-center justify-center min-h-svh gap-4">
-      <div className="flex gap-4">
-        <Button
-          onClick={() => {
-            startCall();
-          }}
-        >
-          Start Call
-        </Button>
-
-        <Button
-          onClick={() => {
-            endCall();
-          }}
-          variant="destructive"
-        >
-          End Call
-        </Button>
-      </div>
-
-      <p>isConnecting: {`${isConnecting}`}</p>
-      <p>isConnected: {`${isConnected}`}</p>
-      <p>isSpeaking: {`${isSpeaking}`}</p>
-      <p>Transcript: {JSON.stringify(transcript, null, 2)}</p>
-    </div>
-  );
+  return <WidgetView organizationId={organizationId} />;
 }
